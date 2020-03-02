5d617209d14a3f9d9576d1a63d19edf0f2ff9612
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-02 14:28:46 +0530

    failing test

--- test/testSum.js
+++ test/testSum.js
@@ -3,6 +3,6 @@ const sum = require('../sum');
 
 describe('sum', () => {
   it('should add two numbers', () => {
-    assert.strictEqual(sum(2, 3), 5);
+    assert.strictEqual(sum(2, 3), 6);
   });
 });
