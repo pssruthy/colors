ec75b1fb5a2002cb83e4702dbed06d5da3171485
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-02 12:01:08 +0530

    added sum function

---
+++ test/testSum.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const sum = require('../sum');
+
+describe('sum', () => {
+  it('should add two numbers', () => {
+    assert.strictEqual(sum(2, 3), 5);
+  });
+});
